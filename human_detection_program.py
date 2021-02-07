a4908c4938854618d56e15c6be60b65d82f42d3f
Santanu Biswas <69161246+Santanu29@users.noreply.github.com>
2021-02-07 13:22:21 +0530

    added icon and increased acurracy
    Reformated the file, the changes are made in variable name. winStride is converted to 8,8. Converted frame to grayscale before impementing hogdescriptor to identify humans.

--- human_detection_program.py
+++ human_detection_program.py
@@ -5,29 +5,32 @@ import tkinter as tk
 from tkinter import filedialog
 
 # Trained model for human detection
-HOGCV = cv2.HOGDescriptor()
-HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
 
 # Detect human in a frame and make a rectangular box around them
 def detect(frame):
-    bounding_box_cordinates, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bounding_box_coordinates, weights = hog.detectMultiScale(gray, winStride=(8, 8), padding=(8, 8), scale=1.03)
 
     person = 1
-    for x, y, w, h in bounding_box_cordinates:
+    for x, y, w, h in bounding_box_coordinates:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
-        cv2.putText(frame, f'Person {person}', (x, y-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, f'Person {person}', (x, y - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
         person += 1
 
     cv2.putText(frame, 'Status : Detecting ', (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
     cv2.putText(frame, f'Total Persons : {person - 1}', (20, 50), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
     cv2.imshow('Output', frame)
 
+
 # Detect human in an image
 def detectInImage():
     path = filedialog.askopenfilename(initialdir="/", title="Select File",
-                                      filetypes=(('JPG (*.jpg;*.jpeg)',('*.jpg','*.jpeg')),
-                                                 ('GIF (*.gif)','*.gif'), ('PNG (*.png)','*.png'),
-                                                 ('All Images Files', ('*.jpg','*.jpeg','*.gif','*.png'))))
+                                      filetypes=(('JPG (*.jpg;*.jpeg)', ('*.jpg', '*.jpeg')),
+                                                 ('GIF (*.gif)', '*.gif'), ('PNG (*.png)', '*.png'),
+                                                 ('All Images Files', ('*.jpg', '*.jpeg', '*.gif', '*.png'))))
     img = cv2.imread(path)
     img = imutils.resize(img, width=min(800, img.shape[1]))
     detect(img)
@@ -35,17 +38,17 @@ def detectInImage():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 # Detect human in a video    
 def detectInVideo():
     path = filedialog.askopenfilename(initialdir="/", title="Select File",
-                                      filetypes=(('MP4 Video File (*mp4)','*.mp4'),
-                                                 ('MKV Video File (*.mkv)','*.mkv'),('All Files','*.*')))
+                                      filetypes=(('MP4 Video File (*mp4)', '*.mp4'),
+                                                 ('MKV Video File (*.mkv)', '*.mkv'), ('All Files', '*.*')))
     video = cv2.VideoCapture(path)
 
     while True:
         isTrue, frame = video.read()
         frame = imutils.resize(frame, width=min(800, frame.shape[1]))
-        # cv2.imshow("Video", frame)
         detect(frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -53,6 +56,7 @@ def detectInVideo():
 
     video.release()
     cv2.destroyAllWindows()
+
 
 # Detect human in a live video through webcam    
 def detectInCamera():
@@ -67,6 +71,7 @@ def detectInCamera():
 
     live.release()
     cv2.destroyAllWindows()
+
 
 # GUI design for input selection    
 def gui():
@@ -96,12 +101,7 @@ def gui():
 
     root.mainloop()
 
+
 # Start of program
-if __name__=="__main__":
+if __name__ == "__main__":
     gui()
-
-
-
-
-
-
